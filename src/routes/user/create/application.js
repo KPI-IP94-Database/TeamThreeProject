8271e529145b61b70230c2cfb93e4251847f8ed9
Illia Aprol <asdf21072107@gmail.com>
2021-06-03 11:04:11 +0300

    Add application and application-list functionality

--- src/routes/user/create/application.js
+++ src/routes/user/create/application.js
@@ -1,9 +1,53 @@
 'use strict';
 
+// General scheme
+const schemeProps = {
+  user_id: { type: 'number' },
+  speciality_id: { type: 'number' },
+  priority: { type: 'string' },
+};
+
+const requiredProps = Object.keys(schemeProps);
+const acceptedProps = Object.keys(schemeProps);
+
 module.exports = (url) => async (fastify) => {
   fastify.route({
     method: 'POST',
     url,
-    handler: async (request, reply) => ({ hello: 'world' }),
+    schema: {
+      body: {
+        type: 'object',
+        propertyNames: { enum: acceptedProps },
+        required: requiredProps,
+        properties: schemeProps,
+      },
+    },
+
+    handler: async (request, reply) => {
+      const priorityValues = ['1', '2', '3', '4', '5', '6', '7', 'K'];
+      if (!priorityValues.includes(request.body.priority)) {
+        reply.code(400).send({
+          statusCode: 400,
+          error: 'Bar request',
+          message: 'Priority value is invalid',
+        });
+        return;
+      }
+
+      const insertedObj = {};
+
+      for (const key of Object.keys(request.body)) {
+        insertedObj[key] = request.body[key];
+      }
+
+      insertedObj.status = 'RECEIVED';
+
+      await fastify.knex('application').insert(insertedObj);
+
+      reply.code(201).send({
+        statusCode: 201,
+        message: 'Application has been added to DB',
+      });
+    },
   });
 };
